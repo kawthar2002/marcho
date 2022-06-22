b4cad36f69beef92f87fadce22aa88286b28a408
kawthar2002 <danilova.ann@inbox.ru>
2022-06-22 18:15:58 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,32 @@
-$(function(){
-    $('.top-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true, 
-        autoplaySpeed: 1000
-    });
+$(function () {
+  $(".top-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+
+const popup = document.querySelector(".popup");
+const button = document.querySelector(".video-popup__link");
+const video = document.querySelector(".popup-inner__video");
+
+button.addEventListener("click", (film) => {
+  film.preventDefault();
+  popup.classList.add("displayed");
+});
+popup.addEventListener("click", (event) => {
+  const popupInner = document.querySelector(".popup-inner");
+  if (event.composedPath().includes(popupInner)) {
+    return;
+  }
+
+  if (popup && popup.classList.contains("displayed")) {
+    popup.classList.remove("displayed");
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+    }
+  }
 });
